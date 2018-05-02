5efcff3ed331b38e8e071d7d3f68608e79bc124f
Casey Goldman <casegoldman@gmail.com>
2018-05-01 20:37:45 -0700

    Save method

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -66,7 +66,8 @@ class App extends Component {
             <Playlist playlistName={this.state.playlistName}
               playlistTracks={this.state.playlistTracks}
               onRemove={this.removeTrack}
-              onNameChange={this.updatePlaylistName} />
+              onNameChange={this.updatePlaylistName}
+              onSave={this.savePlaylist} />
           </div>
         </div>
       </div>
