5a237be9caad382bd5ac06c1dd82ca2bd4052cdb
Casey Goldman <casegoldman@gmail.com>
2018-05-01 19:25:16 -0700

    addTrack method to Track component

--- src/components/Track/Track.js
+++ src/components/Track/Track.js
@@ -2,6 +2,16 @@ import React from 'react';
 import './Track.css';
 
 class Track extends React.Component {
+  constructor(props) {
+    super(props);
+
+    
+  }
+
+  addTrack() {
+    this.props.onAdd(this.props.track)
+  }
+
   renderAction () {
     if (this.props.isRemoval) {
       return '-';
