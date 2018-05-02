8e0506b17e1c644e921b70218ec2a6e10845b6b9
Casey Goldman <casegoldman@gmail.com>
2018-05-02 12:57:21 -0700

    Spotify search method

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -15,7 +15,27 @@ const Spotify = {
       window.location = `https://accounts.spotify.com/authorize?client_id=${client_id}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirect_uri}`;
     }
   }
+  search (term){
+    // returns a promise that will eventually resolve to the list of tracks from the search
+    fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`,{
+      headers: {
+        Authorization: `Bearer ${accessToken}`
+      }
+    }).then(response => {
+      return response.json();
+    }).then(jsonResponse => {
+      if(jsonResponse) {
+        return jsonResponse.items.map(track => ({
+          id: track.id,
+          name: track.name,
+          artist: track.artist[0].name,
+          album: track.album.name,
+          uri: track.uri
+        }));
+      }
+      return [];
+    })
+  }
 };
-
 
 export default Spotify;
