ab6ef086fa83d1a3ce325f2d7572bc8b3fc5f09e
Casey Goldman <casegoldman@gmail.com>
2018-05-02 12:26:53 -0700

    Obtain Sportify access token from user

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,11 +1,21 @@
 const accessToken = '';
+const client_id = process.env.SPOTIFY_CLIENT_ID;
+const redirect_uri = 'http://localhost:3000/';
 
 const Spotify = {
-  getAccessToken: function(){
+  getAccessToken() {
     if (accessToken) {
       return accessToken;
+    } else if (window.location.href.includes('access_token')) {
+      accessToken = window.location.href.match(/access_token=([^&]*)/);
+      const expiresIn = window.location.href.match(/expires_in=([^&]*)/);
+      window.setTimeout(() => accessToken = '', expiresIn * 1000);
+      window.history.pushState('Access Token', null, '/');
+    } else {
+      window.location = `https://accounts.spotify.com/authorize?client_id=${client_id}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirect_uri}`;
     }
   }
 };
+
 
 export default Spotify;
