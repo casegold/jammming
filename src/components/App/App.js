273aa71ecd65e5604975034bfe932155f818dc3c
Casey Goldman <casegoldman@gmail.com>
2018-05-01 20:52:36 -0700

    Set up search method

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -31,6 +31,7 @@ class App extends Component {
     this.removeTrack = this.removeTrack.bind(this);
     this.updatePlaylistName = this.updatePlaylistName.bind(this);
     this.savePlaylist = this.savePlaylist.bind(this);
+    this.search = this.search.bind(this);
   }
 
   addTrack(track) {
@@ -54,13 +55,16 @@ class App extends Component {
     //after passing to Spotify it will reset playlist name and tracks array
     return {playlistName: this.state.playlistTracks.name, trackURIs: trackURIs};
   }
+  search (term) {
+    console.log(term);
+  }
 
   render() {
     return (
       <div>
         <h1>Ja<span className="highlight">mmm</span>ing</h1>
         <div className="App">
-          <SearchBar />
+          <SearchBar onSearch={this.search} />
           <div className="App-playlist">
             <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack} />
             <Playlist playlistName={this.state.playlistName}
